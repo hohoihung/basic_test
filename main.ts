c3051aeeb28d97c0cc468ee1ae2764b34298eb55
hohoihung <w10546.ho@gmail.com>
2023-02-14 16:35:05 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
+serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    basic.clearScreen()
+    basic.showString(serial.readUntil(serial.delimiters(Delimiters.NewLine)))
+    basic.pause(200)
+})
 let p0 = 0
 pins.setPull(DigitalPin.P0, PinPullMode.PullDown)
 basic.showIcon(IconNames.SmallHeart)
+serial.redirect(
+SerialPin.P13,
+SerialPin.P12,
+BaudRate.BaudRate9600
+)
 basic.forever(function () {
+    basic.clearScreen()
     p0 = pins.digitalReadPin(DigitalPin.P0)
     basic.showNumber(p0)
     basic.pause(500)
-    basic.clearScreen()
+    basic.showIcon(IconNames.Yes)
 })
